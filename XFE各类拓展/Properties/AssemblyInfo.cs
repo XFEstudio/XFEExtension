61884b4c08e575b6418f34dbdb4a17fae5db9de0
xfe18827675401 <xfe18827675401@126.com>
2023-10-31 15:41:59 +0800

    修改程序集信息

--- XFE各类拓展/Properties/AssemblyInfo.cs
+++ XFE各类拓展/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Json拓展类")]
+[assembly: AssemblyTitle("XFE各类拓展")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Json拓展类")]
+[assembly: AssemblyCompany("XFE寰宇朽力网络科技")]
+[assembly: AssemblyProduct("XFE各类拓展")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
