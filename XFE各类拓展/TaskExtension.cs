eecac1f4406932c5f7b704360d227a38546b51ba
XFEstudio <mail@xfegzs.com>
2023-11-03 11:34:31 +0800

    XFE等待器新增任务ID，便于管理

--- XFE各类拓展/TaskExtension.cs
+++ XFE各类拓展/TaskExtension.cs
@@ -14,8 +14,9 @@ namespace XFE各类拓展.TaskExtension
         /// 传入值并终止等待器
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <param name="taskId">任务ID</param>
         /// <param name="result">目标等待器返回值</param>
-        public delegate void EndTaskTrigger<T>(T result);
+        public delegate void EndTaskTrigger<T>(T result, string taskId = "DEFAULTTASKID");
         private static long CTimeCounter = 0;
         #region 新建任务并开始
         /// <summary>
@@ -96,12 +97,18 @@ namespace XFE各类拓展.TaskExtension
         {
             private TaskCompletionSource<T> WaitTaskSource { get; set; } = new TaskCompletionSource<T>();
             /// <summary>
+            /// 任务ID
+            /// </summary>
+            public string TaskId { get; set; }
+            /// <summary>
             /// 结束任务并返回
             /// </summary>
-            /// <param name="result"></param>
-            public void EndTask(T result)
+            /// <param name="result">返回结果</param>
+            /// <param name="taskId">任务ID</param>
+            public void EndTask(T result, string taskId)
             {
-                WaitTaskSource.SetResult(result);
+                if (taskId == TaskId)
+                    WaitTaskSource.SetResult(result);
             }
             /// <summary>
             /// 获取等待器
@@ -115,9 +122,11 @@ namespace XFE各类拓展.TaskExtension
             /// XFE等待器
             /// </summary>
             /// <param name="endTaskAndReturn">触发任务结束事件</param>
-            public XFEWaitTask(ref EndTaskTrigger<T> endTaskAndReturn)
+            /// <param name="taskId">任务ID</param>
+            public XFEWaitTask(ref EndTaskTrigger<T> endTaskAndReturn, string taskId = "DEFAULTTASKID")
             {
                 endTaskAndReturn += EndTask;
+                TaskId = taskId;
             }
         }
     }
