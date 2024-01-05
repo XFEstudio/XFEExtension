fc6f88b53dde491b67d22272732b5265d898a7e8
XFEstudio <mail@xfegzs.com>
2024-01-06 02:46:30 +0800

    添加部分xml注释 修订版本号

--- XFE各类拓展.NetCore/PermissionExtension/CurrentPermissionState.cs
+++ XFE各类拓展.NetCore/PermissionExtension/CurrentPermissionState.cs
@@ -2,10 +2,22 @@
 
 public static partial class AdministratorPermission
 {
+    /// <summary>
+    /// 当前UAC权限状态
+    /// </summary>
     public enum CurrentPermissionState
     {
+        /// <summary>
+        /// 已取得管理员权限
+        /// </summary>
         Administration,
+        /// <summary>
+        /// 管理员权限请求被拒绝
+        /// </summary>
         PermissionDenied,
+        /// <summary>
+        /// 正常状态
+        /// </summary>
         Normal
     }
 }
