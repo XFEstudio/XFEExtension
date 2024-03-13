c99305c6a07fc1de74358384616a2ed34bd64de2
XFEstudio <mail@xfegzs.com>
2024-03-14 02:14:47 +0800

    X和XL方法添加别名重载

--- XFEExtension.NetCore/StringExtension/StringExtension.cs
+++ XFEExtension.NetCore/StringExtension/StringExtension.cs
@@ -259,16 +259,18 @@ public static partial class StringExtension
     /// 分析对象并输出到控制台
     /// </summary>
     /// <param name="obj">待分析的对象</param>
+    /// <param name="remarkName">对象别名</param>
     /// <param name="onlyProperty"></param>
     /// <param name="onlyPublic"></param>
-    public static void X(this object? obj, bool onlyProperty = true, bool onlyPublic = true) => Console.WriteLine(XFEConverter.GetObjectInfo(obj?.GetType().Name, ObjectPlace.Main, 0, obj?.GetType(), obj, onlyProperty, onlyPublic).OutPut());
+    public static void X(this object? obj, string remarkName = "分析对象", bool onlyProperty = true, bool onlyPublic = true) => Console.WriteLine(XFEConverter.GetObjectInfo(remarkName, ObjectPlace.Main, 0, obj?.GetType(), obj, onlyProperty, onlyPublic).OutPut());
     /// <summary>
     /// 分析对象并输出到跟踪输出
     /// </summary>
     /// <param name="obj">待分析的对象</param>
+    /// <param name="remarkName">对象别名</param>
     /// <param name="onlyProperty"></param>
     /// <param name="onlyPublic"></param>
-    public static void XL(this object? obj, bool onlyProperty = true, bool onlyPublic = true) => Trace.WriteLine(XFEConverter.GetObjectInfo(obj?.GetType().Name, ObjectPlace.Main, 0, obj?.GetType(), obj, onlyProperty, onlyPublic).OutPut());
+    public static void XL(this object? obj, string remarkName = "分析对象", bool onlyProperty = true, bool onlyPublic = true) => Trace.WriteLine(XFEConverter.GetObjectInfo(remarkName, ObjectPlace.Main, 0, obj?.GetType(), obj, onlyProperty, onlyPublic).OutPut());
     /// <summary>
     /// 字符串实际显示的长度
     /// </summary>
