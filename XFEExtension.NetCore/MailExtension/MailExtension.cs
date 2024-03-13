faa410a39e38f40ba90a31b8ae32899f3fff2009
XFEstudio <mail@xfegzs.com>
2024-03-14 01:31:28 +0800

    更改邮箱拓展

--- XFEExtension.NetCore/MailExtension/MailExtension.cs
+++ XFEExtension.NetCore/MailExtension/MailExtension.cs
@@ -12,19 +12,23 @@ public static class MailExtension
     /// 发送邮件
     /// </summary>
     /// <param name="content">内容</param>
+    /// <param name="email">邮箱</param>
+    /// <param name="host">邮箱主机</param>
+    /// <param name="displayName">显示名称</param>
     /// <param name="password">密码</param>
     /// <param name="subTitle">标题</param>
     /// <param name="to">目标邮箱地址</param>
+    /// <param name="port">端口</param>
     /// <exception cref="XFEMailException"></exception>
-    public static void SendEmail(this string content, string password, string subTitle, string to)
+    public static void SendEmail(this string content, string email, string host, string password, string displayName, string subTitle, string to, int port = 587)
     {
         try
         {
-            using var client = new SmtpClient("smtp.exmail.qq.com", 587);
+            using var client = new SmtpClient(host, port);
             client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("mail@xfegzs.com", password);
+            client.Credentials = new NetworkCredential(email, password);
             client.EnableSsl = true;
-            MailAddress fromMailAddress = new("mail@xfegzs.com", "XFEMail");
+            MailAddress fromMailAddress = new(email, displayName);
             MailMessage message = new()
             {
                 From = fromMailAddress
@@ -32,41 +36,7 @@ public static class MailExtension
             message.To.Add(to);
             message.Subject = subTitle;
             message.Body = content;
-            // 发送邮件
             client.Send(message);
-        }
-        catch (Exception ex)
-        {
-            throw new XFEMailException("邮件发送错误！" + ex.Message);
-        }
-    }
-    /// <summary>
-    /// 发送邮件
-    /// </summary>
-    /// <param name="password">密码</param>
-    /// <param name="name">名称</param>
-    /// <param name="subTitle">标题</param>
-    /// <param name="content">内容</param>
-    /// <param name="to">目标邮箱地址</param>
-    /// <exception cref="XFEMailException"></exception>
-    public static void SendEmail(this string content, string password, string name, string subTitle, string to)
-    {
-        try
-        {
-            using var client = new SmtpClient("smtp.exmail.qq.com", 587);
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("mail@xfegzs.com", password);
-            client.EnableSsl = true;
-            MailAddress fromMailAddress = new("mail@xfegzs.com", name);
-            MailMessage message = new()
-            {
-                From = fromMailAddress
-            };
-            message.To.Add(to);
-            message.Subject = subTitle;
-            message.Body = content;
-            // 发送邮件
-            client.SendAsync(message, null);
         }
         catch (Exception ex)
         {
