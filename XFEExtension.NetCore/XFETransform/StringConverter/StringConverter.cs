54b3b6c7ea71c6eaa2601af3f02d4ff653826adc
XFEstudio <mail@xfegzs.com>
2024-04-09 22:26:54 +0800

    为StringConverter类添加静态属性

--- XFEExtension.NetCore/XFETransform/StringConverter/StringConverter.cs
+++ XFEExtension.NetCore/XFETransform/StringConverter/StringConverter.cs
@@ -8,6 +8,14 @@ namespace XFEExtension.NetCore.XFETransform.StringConverter;
 public abstract class StringConverter : IStringConverter
 {
     /// <summary>
+    /// 对象分析注入
+    /// </summary>
+    public static ObjectAnalyzer ObjectAnalyzer { get; } = new();
+    /// <summary>
+    /// Json转换注入
+    /// </summary>
+    public static JsonTransformer JsonTransformer { get; } = new();
+    /// <summary>
     /// 输出对象信息
     /// </summary>
     /// <param name="objectInfo"></param>
