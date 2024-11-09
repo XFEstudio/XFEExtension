c679a45cc7d1d805c2f2458b45b679d814a2c86f
XFEstudio <mail@xfegzs.com>
2024-11-10 04:09:27 +0800

    add-新增流程控制语句，现在，你可以通过抛出StopAction异常来执行方法并抛出异常，从而穿插在代码中

---
+++ XFEExtension.NetCore/XFEExtension/AllException/StopAction.cs
@@ -0,0 +1,37 @@
+﻿namespace XFEExtension.NetCore;
+
+/// <summary>
+/// 流程控制：停止并执行方法
+/// </summary>
+public class StopAction : Exception
+{
+    /// <summary>
+    /// 执行方法并抛出异常
+    /// </summary>
+    /// <param name="stopAction">需要执行的方法</param>
+    /// <param name="message">异常消息</param>
+    public StopAction(Action stopAction, string? message = null) : base(message) => stopAction.Invoke();
+
+    /// <summary>
+    /// 执行方法并抛出异常
+    /// </summary>
+    /// <param name="stopAction">需要执行的方法</param>
+    /// <param name="message">异常消息</param>
+    /// <param name="innerException">内部错误</param>
+    public StopAction(Action stopAction, string? message, Exception? innerException) : base(message, innerException) => stopAction.Invoke();
+
+    /// <summary>
+    /// 执行异步方法并抛出异常
+    /// </summary>
+    /// <param name="stopAction">需要执行的方法</param>
+    /// <param name="message">异常消息</param>
+    public StopAction(Func<Task> stopAction, string? message = null) : base(message) => stopAction.Invoke().Wait();
+
+    /// <summary>
+    /// 执行异步方法并抛出异常
+    /// </summary>
+    /// <param name="stopAction">需要执行的方法</param>
+    /// <param name="message">异常消息</param>
+    /// <param name="innerException">内部错误</param>
+    public StopAction(Func<Task> stopAction, string? message, Exception? innerException) : base(message, innerException) => stopAction.Invoke().Wait();
+}
